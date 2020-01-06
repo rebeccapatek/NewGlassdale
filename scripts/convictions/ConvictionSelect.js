a56b8b577cdcc79ef92716f0600e8019267b2977
rebeccapatek <rebeccapatek@gmail.com>
2020-01-05 19:04:16 -0600

    added eventHub to convictionSelect

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -2,10 +2,21 @@ import { useConvictions } from "./ConvictionProvider.js"
 
 // Get a reference to the DOM element where the <select> will be rendered
 const contentTarget = document.querySelector(".filters__crime")
-
+const eventHub = document.querySelector(".container")
 const ConvictionSelect = () => {
     // Get all convictions from application state
     const convictions = useConvictions()
+    eventHub.addEventListener("change", changeEvent => {
+        if (changeEvent.target.classList.contains("dropdown")) {
+            const selectedConviction = changeEvent.target.value.split("-").join(" ")
+            const message = new CustomEvent("convictionSelected", {
+                detail: {
+                   crime: selectedConviction 
+                }
+            })
+            eventHub.dispatchEvent(message)
+        }
+    })
 
     const render = convictionsCollection => {
         let options = convictionsCollection.map = (conviction => `
